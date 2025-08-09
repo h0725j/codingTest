d4b0a1e2468c4cfe154f404634768660dd675dc4
h0725j <106363851+h0725j@users.noreply.github.com>
2025-08-09 12:59:40 +0900

    [level 1] Title: 소수 찾기, Time: 220.12 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 2; i <= n; i++) {
+            boolean result = true;
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    result = false;
+                    break;
+                }
+            }
+            if (result) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
