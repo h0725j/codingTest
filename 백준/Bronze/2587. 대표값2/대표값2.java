bd946b33c31e1578e3b8945e1b149faaa2a35ad8
h0725j <106363851+h0725j@users.noreply.github.com>
2025-08-14 10:52:43 +0900

    [Bronze II] Title: 대표값2, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+       int[] num = new int[5];
+       int sum = 0;
+       for (int i = 0; i < 5; i++) {
+           num[i] = Integer.parseInt(reader.readLine());
+       }
+
+       for (int i = 0; i < 5; i++) {
+           sum += num[i];
+       }
+
+        System.out.println(sum / num.length);
+
+       Arrays.sort(num);
+        System.out.println(num[num.length / 2]);
+    }
+}
