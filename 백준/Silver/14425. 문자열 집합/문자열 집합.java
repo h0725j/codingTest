285ba651d6c831a0b2bf4fb53a14518938e3ac1c
h0725j <106363851+h0725j@users.noreply.github.com>
2025-08-22 11:39:12 +0900

    [Silver IV] Title: 문자열 집합, Time: 456 ms, Memory: 32104 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+        HashSet<String> nList = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            nList.add(tokenizer.nextToken());
+        }
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            String target = tokenizer.nextToken();
+            if (nList.contains(target)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
