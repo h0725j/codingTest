93e77e41056c789e17a1ebb3224f9279094aa47f
h0725j <106363851+h0725j@users.noreply.github.com>
2025-08-25 11:51:52 +0900

    [Silver IV] Title: 대칭 차집합, Time: 616 ms, Memory: 68352 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+
+        HashSet<Integer> a = new HashSet<>();
+
+        tokenizer = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < n; i++) {
+            a.add(Integer.parseInt(tokenizer.nextToken()));
+        }
+
+        int count = 0;
+        tokenizer = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(tokenizer.nextToken());
+            if (!a.contains(num)) {
+                count++;
+            } else {
+                a.remove(num);
+            }
+        }
+
+        count += a.size();
+
+        System.out.println(count);
+    }
+}
