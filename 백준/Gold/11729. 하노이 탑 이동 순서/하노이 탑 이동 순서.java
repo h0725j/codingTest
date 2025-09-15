b9df7f729229bb53eb4bb853fff88850ba223871
h0725j <106363851+h0725j@users.noreply.github.com>
2025-09-15 12:11:36 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 508 ms, Memory: 46852 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -4,14 +4,14 @@ import java.io.InputStreamReader;
 
 public class Main {
     static StringBuilder builder = new StringBuilder();
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(reader.readLine());
 
         // 총 이동 횟수 출력(최소 이동 횟수 공식: 2^n - 1
-        builder.append((int)Math.pow(2, n) - 1).append("\n");
+        builder.append((1 << n) - 1).append('\n');
 
         // 이동 과정 - 1에서 3번 기둥으로 이동(2번 기둥 경유)
         move(n, 1, 3, 2);
@@ -23,7 +23,7 @@ public class Main {
     public static void move(int n, int from, int to, int via) {
         // 원판이 1개면 출발 기둥에서 도착 기둥으로 직접 이동
         if (n == 1) {
-            builder.append(from).append(" ").append(to).append("\n");
+            builder.append(from).append(' ').append(to).append('\n');
             return;
         }
 
@@ -31,7 +31,7 @@ public class Main {
         move(n - 1, from , via, to);
 
         // 가장 큰 원판을 목적지로 이동
-        builder.append(from).append(" ").append(to).append("\n");
+        builder.append(from).append(' ').append(to).append('\n');
 
         // n - 1개 원판을 최종 목적지로 이동
         move(n - 1, via, to, from);
