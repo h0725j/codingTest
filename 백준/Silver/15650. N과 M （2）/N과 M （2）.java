c1581008536c8bc7cfb3d5c24e3130c3dc0e1cc4
h0725j <106363851+h0725j@users.noreply.github.com>
2025-09-22 11:13:33 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        n = Integer.parseInt(tokenizer.nextToken());
+        m = Integer.parseInt(tokenizer.nextToken());
+
+        visited = new boolean[n + 1];
+        arr = new int[m];
+
+        backtrack(1, 0);
+        System.out.println(result);
+    }
+
+    public static void backtrack(int start, int depth) {
+        if (depth == m) {
+            for (int j : arr) {
+                result.append(j).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+
+            backtrack(i + 1, depth + 1);
+        }
+    }
+}
