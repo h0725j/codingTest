69b14db4412524a776acfb230e985b82f681c1b0
h0725j <106363851+h0725j@users.noreply.github.com>
2025-10-23 10:53:11 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 676 ms, Memory: 106016 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -6,8 +6,20 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder builder = new StringBuilder();
         String input = reader.readLine();
         char[] s = input.toCharArray();
+
+        int[][] prefix = new int[26][s.length + 1];
+        for (int i = 0; i < s.length; i++) {
+            int index = s[i] - 'a';
+
+            for (int j = 0; j < 26; j++) {
+                prefix[j][i + 1] = prefix[j][i];
+            }
+
+            prefix[index][i + 1]++;
+        }
 
         int q = Integer.parseInt(reader.readLine());
         for (int i = 0; i < q; i++) {
@@ -16,14 +28,11 @@ public class Main {
             int l = Integer.parseInt(tokenizer.nextToken());
             int r = Integer.parseInt(tokenizer.nextToken());
 
-            int cnt = 0;
-            for (int j = l; j <= r; j++) {
-                if (a == s[j]) {
-                   cnt++;
-                }
-            }
-
-            System.out.println(cnt);
+            int index = a - 'a';
+            int cnt = prefix[index][r + 1] - prefix[index][l];
+            builder.append(cnt).append("\n");
         }
+
+        System.out.println(builder);
     }
 }
