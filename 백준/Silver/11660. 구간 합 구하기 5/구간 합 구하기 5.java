40ee719905652eb0d0f6668ad17671693a0bd784
h0725j <106363851+h0725j@users.noreply.github.com>
2025-10-27 10:41:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 720 ms, Memory: 122324 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -12,29 +12,29 @@ public class Main {
         int n = Integer.parseInt(tokenizer.nextToken());
         int m = Integer.parseInt(tokenizer.nextToken());
 
-        int[][] matrix = new int[n][n];
+        int[][] arr = new int[n][n];
         for (int i = 0; i < n; i++) {
-            StringTokenizer tokenizer1 = new StringTokenizer(reader.readLine());
+            tokenizer = new StringTokenizer(reader.readLine());
             for (int j = 0; j < n; j++) {
-                matrix[i][j] = Integer.parseInt(tokenizer1.nextToken());
+                arr[i][j] = Integer.parseInt(tokenizer.nextToken());
             }
         }
 
         int[][] prefix = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + matrix[i - 1][j - 1];
+                prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + arr[i - 1][j - 1];
             }
         }
 
         for (int i = 0; i < m; i++) {
-            StringTokenizer tokenizer2 = new StringTokenizer(reader.readLine());
-            int x1 = Integer.parseInt(tokenizer2.nextToken());
-            int y1 = Integer.parseInt(tokenizer2.nextToken());
-            int x2 = Integer.parseInt(tokenizer2.nextToken());
-            int y2 = Integer.parseInt(tokenizer2.nextToken());
+            tokenizer = new StringTokenizer(reader.readLine());
+            int x1 = Integer.parseInt(tokenizer.nextToken());
+            int y1 = Integer.parseInt(tokenizer.nextToken());
+            int x2 = Integer.parseInt(tokenizer.nextToken());
+            int y2 = Integer.parseInt(tokenizer.nextToken());
 
-            builder.append(prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1]).append('\n');
+            builder.append(prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1]).append("\n");
         }
 
         System.out.println(builder);
